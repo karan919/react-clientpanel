38eea1b8f35e1011f005508382867bae3c5f87ae
Karan Mahajan <karanmahajan919@gmail.com>
2019-02-08 01:48:41 +0530

    Updates

---
+++ precache-manifest.24ce5dbd072b696b958275d6f49e5d1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "535c884df0dc8503b599fb04a1e57161",
+    "url": "/react-clientpanel/static/media/dev.535c884d.jpg"
+  },
+  {
+    "revision": "15f6544d8b90a7a1bd35",
+    "url": "/react-clientpanel/static/js/runtime~main.15f6544d.js"
+  },
+  {
+    "revision": "8fd35fb9c8dbf29a4f2c",
+    "url": "/react-clientpanel/static/js/main.8fd35fb9.chunk.js"
+  },
+  {
+    "revision": "3c4735cf1812bc867c39",
+    "url": "/react-clientpanel/static/js/1.3c4735cf.chunk.js"
+  },
+  {
+    "revision": "8fd35fb9c8dbf29a4f2c",
+    "url": "/react-clientpanel/static/css/main.97f649d5.chunk.css"
+  },
+  {
+    "revision": "1d9000cf0b9b823150db8cd9ba20019c",
+    "url": "/react-clientpanel/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-clientpanel/precache-manifest.388ee60fe3ae7fe14ee915d1a9842e75.js"
+  "/react-clientpanel/precache-manifest.24ce5dbd072b696b958275d6f49e5d1a.js"
 );
 
 workbox.clientsClaim();
